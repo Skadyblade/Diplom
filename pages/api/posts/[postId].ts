698862eeb0a13f22d55fbc725302c9423bdf953f
Skadyblade <60780056+Skadyblade@users.noreply.github.com>
2024-04-22 20:00:27 +0300

    Update [postId].ts

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -28,7 +28,7 @@ export default async function handler(
                         user: true
                     },
                     orderBy: {
-                        createAt: 'desc'
+                        createdAt: 'desc'
                     }
                 }
             }
